70086670a8dff49fbee68428b55d58c293f0066c
Bryant MacMahon <bryantmac@gmail.com>
2021-11-02 12:59:31 -0700

    Time: 156 ms (79.90%), Space: 57.1 MB (40.30%) - LeetHub

---
+++ sort-an-array/sort-an-array.js
@@ -0,0 +1,37 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortArray = function(arr) {
+    
+    if(arr.length <= 1) return arr;
+    const mid = Math.floor(arr.length/2);
+    const left = sortArray(arr.slice(0, mid));
+    const right = sortArray(arr.slice(mid));
+    return merge(left, right);    
+    
+    
+    function merge(arr1, arr2) {
+    const results = [];
+    let i = 0;
+    let j = 0;
+    while(i < arr1.length && j < arr2.length) {
+        if (arr1[i] < arr2[j]) {
+            results.push(arr1[i]);
+            i++
+        } else {
+            results.push(arr2[j]);
+            j++;
+        }
+    }
+    while(i < arr1.length) {
+        results.push(arr1[i]);
+        i++
+    }
+    while(j < arr2.length) {
+        results.push(arr2[j])
+        j++
+    }
+    return results
+}
+};
