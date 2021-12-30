8f8067aacf8332924854d6c7b20dd606d1164bb8
zataara <bryantmac@gmail.com>
2021-12-29 17:16:19 -0800

    prettifying text

--- Custom/create-note/solution.js
+++ Custom/create-note/solution.js
@@ -2,19 +2,17 @@ function createFrequencyCounter(string) {
   let frequencies = new Map();
   for (let char of string) {
     let charCount = frequencies.get(char) || 0;
-    frequencies.set(char, charCount + 1)
+    frequencies.set(char, charCount + 1);
   }
   return frequencies;
 }
 
-
 function constructNote(str1, str2) {
-
   let charMap1 = createFrequencyCounter(str1);
   let charMap2 = createFrequencyCounter(str2);
 
   for (let letter of charMap1.keys()) {
-    if(charMap1.get(letter) > charMap2.get(letter)) return false;
+    if (charMap1.get(letter) > charMap2.get(letter)) return false;
   }
   return true;
 }
--- Custom/create-note/solution.test.js
+++ Custom/create-note/solution.test.js
@@ -1,10 +1,9 @@
-const constructNote = require('./solution')
+const constructNote = require("./solution");
 
-
-describe("Testing function", ()=> {
-test("functions properly", () => {
-    expect(constructNote('aa', 'abc')).toBe(false);
-    expect(constructNote('abc', 'dcba')).toBe(true);
-    expect(constructNote('aabbcc', 'bcabcaddff')).toBe(true);
+describe("Testing function", () => {
+  test("functions properly", () => {
+    expect(constructNote("aa", "abc")).toBe(false);
+    expect(constructNote("abc", "dcba")).toBe(true);
+    expect(constructNote("aabbcc", "bcabcaddff")).toBe(true);
   });
 });
