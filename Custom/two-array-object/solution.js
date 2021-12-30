9f0d89c57f9f27638737786cedd0d69acdd0b304
zataara <bryantmac@gmail.com>
2021-12-29 17:57:59 -0800

    added new custom algo

--- Custom/two-array-object/solution.js
+++ Custom/two-array-object/solution.js
@@ -1,15 +1,15 @@
 function twoArrayObject(keys, values) {
   let out = {};
-  if(keys.length >= values.length) {
+  if (keys.length >= values.length) {
     let dif = keys.length - values.length;
-    while(dif > 0) {
-      values.push(null)
-      dif --
+    while (dif > 0) {
+      values.push(null);
+      dif--;
     }
   }
-  if(values.length > keys.length){
+  if (values.length > keys.length) {
     let dif = values.length - keys.length;
-    values = values.splice(0, values.length - dif)
+    values = values.splice(0, values.length - dif);
   }
   for (let i = 0; i < keys.length; i++) {
     out[keys[i]] = values[i];
