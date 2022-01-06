0e8fa624293e7bdb6d340793302a472a31ae9f9a
zataara <bryantmac@gmail.com>
2022-01-05 22:59:45 -0800

    updating scratchpads

--- Scratchpads/scratchpad.py
+++ Scratchpads/scratchpad.py
@@ -1,3 +1,25 @@
+# class Solution(object):
+#     def threeSumClosest(self, nums, target):
+#         """
+#         :type nums: List[int]
+#         :type target: int
+#         :rtype: int
+#         """
+#         if len(nums) == 3:
+#           return sum(nums)
+#         toSum = [nums[0], nums[1], nums[2]]
+#         closest = sum(toSum)
+
+#         for i in range(0, len(nums) - 2):
+#           for j in range(1, len(nums) - 1):
+#             for k in range(2, len(nums)):
+#               toSum = [nums[i], nums[j], nums[k]]
+#               summed = sum(toSum)
+#               if summed != target and abs(target - summed) < abs(target - closest):
+#                 closest = summed
+
+#         return closest
+
 # def num_primorial(n):
 #   factorials = []
 #   result = 1
