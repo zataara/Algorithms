5cd64bc65fa038ec7cfeb544138ec9b5d1b90532
zataara <bryantmac@gmail.com>
2022-01-27 23:56:06 -0800

    added LinkedList Node example

---
+++ DSA/Linked-List/node.js
@@ -0,0 +1,37 @@
+class Node {
+  constructor(val, next=null) {
+    this.val = val;
+    this.next = next;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null
+    this.tail = null
+  }
+
+  traverse() {
+    let currentNode = this.head;
+    while(currentNode) {
+      currentNode = currentNode.next
+    }
+  }
+
+  find(val) {
+    let currentNode = this.head;
+    while(currentNode) {
+      if(currentNode.val === val) return true;
+      currentNode = currentNode.next;
+    }
+  }
+
+  append(val) {
+    const newNode = new Node(val);
+    this.tail.next = newNode;
+    this.tail = newNode
+  }
+}
+
+const history = new LinkedList(); 
+history.head = node;
