e9a2b369176228303d1899e539aba1be35c433cb
zataara <bryantmac@gmail.com>
2022-02-13 19:42:58 -0800

    added new unfinished Codewars Kata

---
+++ CodeWars/6kyu/Are-they-the-same/solution.py
@@ -0,0 +1,14 @@
+def comp(a,b):
+  print(a,b)
+  if not a and not b: return True
+  if len(a) == 0 and len(b) == 0: return True
+
+  for num in a:
+    if num**2 not in b:
+      return False
+
+  for num in b:
+    if num**.5 not in a:
+      return False
+  
+  return True
---
+++ CodeWars/6kyu/Are-they-the-same/test.py
@@ -0,0 +1,16 @@
+import test
+from solution import comp
+
+@test.describe("Same")
+def tests():
+    @test.it("Fixed tests")
+    def basics():
+        a1 = [121, 144, 19, 161, 19, 144, 19, 11]
+        a2 = [11*11, 121*121, 144*144, 19*19, 161*161, 19*19, 144*144, 19*19]
+        test.assert_equals(comp(a1, a2), True)
+        a1 = [121, 144, 19, 161, 19, 144, 19, 11]
+        a2 = [11*21, 121*121, 144*144, 19*19, 161*161, 19*19, 144*144, 19*19]
+        test.assert_equals(comp(a1, a2), False)
+        a1 = [121, 144, 19, 161, 19, 144, 19, 11]
+        a2 = [11*11, 121*121, 144*144, 190*190, 161*161, 19*19, 144*144, 19*19]
+        test.assert_equals(comp(a1, a2), False)
